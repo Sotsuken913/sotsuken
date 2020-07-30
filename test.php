598fad46dc185435a63acafe0207d7a9d937423e
Sotsuken913 <66661126+Sotsuken913@users.noreply.github.com>
2020-07-30 14:55:15 +0900

    Create test.php

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="utf8">
+<title>卒業研究</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<html>
+<body>
+<?php
+	try{
+	$pdo = new PDO("mysql:host=localhost; dbname="sotsuken", "root", "12345");
+	$res = $pdo->prepare("insert into sotsuken(No,Username,Email,Count)
+	values(:No,:Username,:Email,:Ninzuu)");
+	$res->bindValue(":No",$_POST['No']);
+	$res->bindValue(":Username",$_POST['Username']);
+	$res->bindValue(":Email",$_POST['Email']);
+	$res->bindValue(":Ninzuu",$_POST['Ninzuu']);
+	$res->execute();
+?>
+</body>
+</html>
